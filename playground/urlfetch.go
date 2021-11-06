b664ec40f3c2a3e69f8f66cd0f3447485bb56bf9
Yizhou Shan <syzwhat@gmail.com>
2021-11-06 13:31:48 -0700

    goroutine based url fetch

--- playground/urlfetch.go
+++ playground/urlfetch.go
@@ -1,25 +1,47 @@
+// Concurrently fetch multiple URLs using goroutine
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
-func main() {
-	for _, url := range os.Args[1:] {
-		fmt.Println(url)
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-		}
-		fmt.Printf("%s", body)
+func fetchURL(url string, c chan string) {
+	start_ts := time.Now()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
 	}
+	// body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+	// }
+	// fmt.Printf("%s", body)
+
+	latency := time.Since(start_ts).Seconds()
+	c <- fmt.Sprintf("%.2fs %s", latency, url)
+}
+
+// E.g., to test, run
+//  go run urlfetch.go https://github.com https://google.com https://baidu.com
+func main() {
+	ch := make(chan string)
+
+	start_ts := time.Now()
+	for _, url := range os.Args[1:] {
+		go fetchURL(url, ch)
+	}
+
+	for range os.Args[1:] {
+		s := <-ch
+		fmt.Println(s)
+	}
+	latency := time.Since(start_ts).Seconds()
+
+	fmt.Printf("Total runtime %fs\n", latency)
 }
